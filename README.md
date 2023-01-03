2fded67a86619d14716b3a4d6536b9f1e7eac144
juadi19 <juadi19@gmail.com>
2023-01-03 11:52:29 -0600

    uploaded scrapper and run code

---
+++ README.md
@@ -0,0 +1,1 @@
+# sd_salary_proj
---
+++ data_collection.py
@@ -0,0 +1,6 @@
+import glassdoor_scraper as gs 
+import pandas as pd 
+
+path = "C:/Users/juadi/Workspace/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist',10, False, path, 5)
